03f0af20392b98adc4dcaaa16a141b3df2dad69d
Droped <droped17@gmail.com>
2025-05-20 23:37:31 +0700

    [add]: EmblaCarousel for HomePage

---
+++ src/components/molecules/EmblaCarousel.tsx
@@ -0,0 +1,80 @@
+import React, { useCallback } from 'react'
+import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
+import {
+    PrevButton,
+    NextButton,
+    usePrevNextButtons
+} from './EmblaCarouselArrowButton'
+import Autoplay from 'embla-carousel-autoplay'
+import useEmblaCarousel from 'embla-carousel-react'
+import { DotButton, useDotButton } from './EmblaCarouselDotButton'
+import Image from 'next/image'
+
+type PropType = {
+    slides: number[]
+    options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+    const { slides, options } = props
+    const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()])
+
+    const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+        const autoplay = emblaApi?.plugins()?.autoplay
+        if (!autoplay) return
+
+        const resetOrStop =
+            autoplay.options.stopOnInteraction === false
+                ? autoplay.reset
+                : autoplay.stop
+
+        resetOrStop()
+    }, [])
+
+    const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+        emblaApi,
+        onNavButtonClick
+    )
+
+    const {
+        prevBtnDisabled,
+        nextBtnDisabled,
+        onPrevButtonClick,
+        onNextButtonClick
+    } = usePrevNextButtons(emblaApi, onNavButtonClick)
+
+    return (
+        <section className="embla">
+            <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container">
+                    {slides.map((index) => (
+                        <div className="embla__slide" key={index}>
+                            <Image alt="relieve" src="https://images.unsplash.com/photo-1616995942688-6ad1d3ee4a60?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width={1500} height={500} className="rounded-4xl" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="embla__controls">
+                <div className="embla__buttons">
+                    <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+                    <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+                </div>
+
+                <div className="embla__dots">
+                    {scrollSnaps.map((_, index) => (
+                        <DotButton
+                            key={index}
+                            onClick={() => onDotButtonClick(index)}
+                            className={'embla__dot'.concat(
+                                index === selectedIndex ? ' embla__dot--selected' : ''
+                            )}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default EmblaCarousel
